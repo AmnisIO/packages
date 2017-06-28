3c7416bc65be03ccea24180f7ae0d1f3da297132
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-28 22:21:00 +0530

    Add type definitions for @amnisio/rivulet

---
+++ rivulet/index.d.ts
@@ -0,0 +1,25 @@
+// Type definitions for rivulet 1.0
+// Project: https://github.com/AmnisIO/rivulet
+// Definitions by: Sudarsan Balaji <https://github.com/artfuldev>
+// Definitions: https://github.com/AmnisIO/packages
+
+export type Byte = number;
+
+interface Rivulet<T> {
+  map: (mapper: (value: T) => T) => Rivulet<T>;
+  mapTo: (value: T) => Rivulet<T>;
+  filter: (predicate: (value: T) => boolean) => Rivulet<T>;
+  take: (count: number) => Rivulet<T>;
+  drop: (count: number) => Rivulet<T>;
+  last: () => Rivulet<T>;
+  sample: (input$: Rivulet<T>) => Rivulet<T>;
+  delay: (delay: number) => Rivulet<T>;
+}
+
+export interface ByteStream extends Rivulet<Byte> {
+
+}
+
+export const periodic: (period: number) => ByteStream;
+export const never: () => ByteStream;
+export const empty: () => ByteStream;
